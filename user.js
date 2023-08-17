ba09e143162515f26245163566809a7ead7ce8f1
DmNick <dmnickb@gmail.com>
2023-08-17 13:25:11 +0200

    0.95 - update

--- user.js
+++ user.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name         Helpdesk / Powiadomienia windows
 // @namespace    Eko-okna
-// @version      0.91
+// @version      0.95
 // @description  Powiadomienia o nowych ticketach.
 // @author       Dominik Banik dominik.banik@ekookna.pl
 // @downloadURL  https://raw.githubusercontent.com/DmNick/helpdeskNotify/main/user.js
 // @updateURL    https://raw.githubusercontent.com/DmNick/helpdeskNotify/main/user.js
 // @match        https://helpdesk/
-// @require      https://raw.githubusercontent.com/DmNick/helpdeskNotify/main/notifications.js
 // @require  	 https://code.jquery.com/jquery-3.7.0.min.js
+// @require      https://raw.githubusercontent.com/DmNick/helpdeskNotify/main/notifications.js
+// @require      https://raw.githubusercontent.com/DmNick/helpdeskNotify/main/Print.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/printThis/1.15.0/printThis.js
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAiJJREFUOE+Vk11Ik2EUx//Pq9t4xbWl9MaimhuIXWREVwXShTg/sPAmGrMmU1sXejEoyA0aJoqWedtNYYmzxL5wlOUHSRdB3teFCG4xa1rhaJhaNnfC593z1sUu5nNzzv9/zvmdhwceRkQM/awKCkagQEG2U5AxdZmYysRtgNEA6mDFKzp2GXT4FqDfk5WRzZSm8sDoOVbTpR1F6SM9OQ+KxvywDowmQFunv+9qswDo3xjA6Alos35j19t3BuS3BSrgZ+0aB+Q9qsN242sNJnSVy4+95kKcKC9FoM2p1QunjSog6fjBTf3QKWx53msNQjsuBDDzsA/XegdRbDaio+087zGNm8HoPijRsMoNOVSBTfc7DSB0rfs6JkPqI1e6/JgdvcnzovFiMLoN+tb0lRvG0EmQwaQB2O8k1txzqPd0YmKoi/vO9j6M3QnwXHm5H4z6QcsX4+qVxiqRdM5qAKHPtnbhxWAn92uagpga7ua5ZeSACvjsWlKv9NSBxLkZDSB0g7cb4XtB+G7cxdEyK7yuGt5zcPQQ2K8gaKX1kwqYbkai+sE/QEY3+gbAGEN1xXEErzQjFovxHuvjErB1P2jFu8gNm83GYzQahcVigSzLiEQisNvt3CciXts5UmoDJeFyFfClZUHbmmtiXhzGvg89cQ5Y8sznOgcptQ5T7BmUj70AwxmW9IGkfHVeEt/1P1z6D6AT3xmAwciLcUi4hKs0+RdlXsVylWyVrQAAAABJRU5ErkJggg==
 // @grant        none
 // ==/UserScript==
@@ -110,8 +112,70 @@
      color: #c6d0dc!important;
     }
 
+    [ng-model='ticket.description'] span {
+     color: #c6d0dc!important;
+    }
+
     #dzwieki > div > label {
      width:200px;
+    }
+
+    .ticket-description {
+     max-height:500px;
+    }
+
+    #naklejka {
+        display: flex;
+        width: 150mm;
+        height: 35mm;
+        font-size: 18px;
+        flex-wrap: wrap;
+        align-content: flex-end;
+        align-items: flex-end;
+        margin: 0;
+        padding: 10px;
+        font-weight:bold;
+        box-sizing: border-box;
+        /*position: absolute;
+        z-index: -1;*/
+    }
+
+    #details-additional-fields {
+     text-align: -webkit-center;
+    }
+
+    .printNazwa {
+     overflow: hidden;
+     display: -webkit-box;
+     -webkit-line-clamp: 2;
+     line-clamp: 2;
+     -webkit-box-orient: vertical;
+    }
+
+    .printOpis, .printPodpis {
+     overflow: hidden;
+     display: -webkit-box;
+     -webkit-line-clamp: 1;
+     line-clamp: 1;
+     -webkit-box-orient: vertical;
+    }
+
+    @page {
+        size: auto;
+        /* auto is the initial value */
+        margin: 0mm;
+        /* this affects the margin in the printer settings */
+    }
+
+    @media print {
+
+        html,
+        body {
+            margin: 0;
+            size: auto;
+            size: landscape;
+            /* size: 100mm 35mm;*/
+        }
     }
 
     @media screen and (max-width: 600px) {
@@ -300,6 +364,15 @@
                         }
                     };
                 }
+                if(url.indexOf("tickets/") > -1 && this.responseURL.split("/").length == '6'){
+                    if (this.readyState == 4) {
+                        if (this.status == 200){
+                            //console.log(this.responseURL.split("/"));
+                            console.log(this);
+                            if(hasPrintLayout()){printLayout(this)};
+                        }
+                    }
+                }
             });
 
             this.addEventListener('error', function() {
@@ -309,6 +382,112 @@
         };
     };
 
+    function printLayout(el){
+        let resp = JSON.parse(el.response);
+        let opis = document.querySelector("#details-additional-fields");
+        if(opis){console.log("wczytane")}else{console.log("nie wczytano");}
+        var tid = setInterval(()=>{
+            var elementExist = $('#details-additional-fields');
+            if (elementExist.length == 1) {
+                functionToLoad();
+            }
+        }, 500);
+
+        function functionToLoad(){
+            clearInterval(tid);
+
+            console.log("TERA WCZYTANO");
+            console.log(document.querySelector("#details-additional-fields"));
+            console.log(resp);
+            console.log(JSON.parse(resp.customFields).fields);
+            let tab = [];
+            document.querySelectorAll(".details-additional-fields__item").forEach(el=>{
+                tab[el.firstElementChild.innerText] = el.lastElementChild.innerText;
+            });
+
+            if(!tab["Miejsce pracy:"] && !document.querySelector("#Miejsce\\ pracy\\:")){
+                let inputMP = document.createElement("input");
+                inputMP.id = "Miejsce pracy:";
+                inputMP.placeholder = "Miejsce pracy";
+                inputMP.addEventListener('keyup',(el)=>{
+                    console.log(el);
+                    document.querySelectorAll(".printOpisMP")[0].innerHTML = el.target.value;
+                });
+                document.querySelector("#details-additional-fields").append(inputMP);
+            }
+
+            if(!tab["Numer pomieszczenia:"] && !document.querySelector("#Numer\\ pomieszczenia\\:")){
+                let inputMP = document.createElement("input");
+                inputMP.id = "Numer pomieszczenia:";
+                inputMP.placeholder = "Numer pomieszczenia";
+                inputMP.addEventListener('keyup',(el)=>{
+                    console.log(el);
+                    document.querySelectorAll(".printOpisNP")[0].innerHTML = el.target.value;
+                });
+                document.querySelector("#details-additional-fields").append(inputMP);
+            }
+
+            if(!tab["Numer kontaktowy:"] && !document.querySelector("#Numer\\ kontaktowy\\:")){
+                let inputMP = document.createElement("input");
+                inputMP.id = "Numer kontaktowy:";
+                inputMP.placeholder = "Numer kontaktowy";
+                inputMP.addEventListener('keyup',(el)=>{
+                    console.log(el);
+                    document.querySelectorAll(".printOpisNK")[0].innerHTML = el.target.value;
+                });
+                document.querySelector("#details-additional-fields").append(inputMP);
+            }
+
+            if(document.querySelector("#naklejka")){
+                let naklejka = document.querySelector("#naklejka");
+                let printButton = document.querySelector("#printButton");
+            }
+            else {
+                let naklejka = document.createElement("div");
+                document.querySelector("[ng-model='ticket.description']").append(naklejka);
+                naklejka.id = 'naklejka';
+                naklejka.style.display = "block";
+                naklejka.style.width = "150mm";
+                naklejka.style.height = "35mm";
+                naklejka.style.fontSize = "30px";
+                let printButton = document.createElement("button");
+                printButton.id = "printButton";
+                printButton.style.color = "#000";
+                printButton.innerHTML = "Drukuj";
+                document.querySelector("#details-additional-fields").append(document.createElement("br"));
+                document.querySelector("#details-additional-fields").append(printButton);
+            }
+            let naklejka = document.querySelector("#naklejka");
+            let printButton = document.querySelector("#printButton");
+            naklejka.innerHTML = `
+            <div class="printNazwa">#${resp.displayId} ${resp.subject}</div>
+            <div class="printOpis"><span class="printOpisMP">${tab["Miejsce pracy:"]??''}</span> <span class="printOpisNP">${tab["Numer pomieszczenia:"]??''}</span></div>
+            <div class="printPodpis">${resp.creatorUser.fullName} <span class="printOpisNK">${tab["Numer kontaktowy:"]??''}<span></div>
+            `;
+
+            printButton.addEventListener('click',(el)=>{
+                console.log(el.target);
+                /*Print(naklejka, {
+                    onStart: function() {
+                        //console.log('onStart', new Date())
+                    },
+                    onEnd: function() {
+                        //console.log('onEnd', new Date())
+                    }
+                })*/
+                //printJS('naklejka', 'html');
+                $("#naklejka").printThis({
+                    importCSS: true,
+                    importStyle: true
+                });
+            });
+        }
+    }
+
+    function createPrintLayout(resp){
+
+    }
+
     function refreshZegary(){
         document.querySelectorAll(".footerContent").forEach((el) => {
             let staraData = el.getAttribute("data-cr");
@@ -317,7 +496,7 @@
             if(minuty>15 && minuty <30){
                 el.closest(".card").style.backgroundColor = "orange";
             } else if (minuty>30){
-                if(!el.closest(".card").classList.contains("warning")){
+                if(!el.closest(".card").classList.contains("warning") && hasAudio()===true){
                     console.log("przypomnienie");
                     audioAlert('przypomnienie30min');
                 }
@@ -353,8 +532,17 @@
             }
     }
 
+    function hasPrintLayout() {
+            if (localStorage.getItem("HP-PrintLayout") == 'true'){
+                return(true);
+            }
+            else {
+                return(false);
+            }
+    }
+
     function refresh10s(){
-        if(document.querySelector(".widoczne")){
+        if(document.querySelector("#layoutNotify.widoczne")){
             refreshList();
         }
     }
@@ -410,7 +598,8 @@
         <div><label for="HP-AudioKrytyczny">Krytyczny priorytet: </label><input class="audio" type="text" placeholder="podaj link do .mp3" id="HP-AudioKrytyczny" /><button class="testMp3">Test</button></div>
         <div><label for="HP-AudioBloker">Bloker priorytet: </label><input class="audio" type="text" placeholder="podaj link do .mp3" id="HP-AudioBloker" /><button class="testMp3">Test</button></div>
         <div><label for="HP-AudioAwaria">Awaria: </label><input class="audio" type="text" placeholder="podaj link do .mp3" id="HP-AudioAwaria" /><button class="testMp3">Test</button></div>
-        <div><label for="HP-OpenLayout">Automatyczny layout: </label><input class="cbox" type="checkbox" id="HP-OpenLayout" /></div>
+        <div><label for="HP-OpenLayout">Auto uruchom layout: </label><input class="cbox" type="checkbox" id="HP-OpenLayout" /></div>
+        <div><label for="HP-PrintLayout">Drukowanie etykietek: </label><input class="cbox" type="checkbox" id="HP-PrintLayout" /></div>
         `;
 
 
@@ -464,6 +653,15 @@
                 }
             });
         });
+
+        document.querySelectorAll("#dzwieki div input").forEach(el=>{
+            if(el.classList.contains('audio')){
+                el.value = localStorage.getItem(el.id)??'';
+            }
+            if(el.classList.contains('cbox')){
+                el.checked = localStorage.getItem(el.id)??'';
+            }
+        });
     }
 
     function openSettings(){
@@ -471,12 +669,14 @@
         //layout.style.top = "0";
         settings.classList.remove("niewidoczne");
         settings.classList.add("widoczne");
-        document.querySelector("#HP-AudioNiski").value = localStorage.getItem("HP-AudioNiski")??'';
-        document.querySelector("#HP-AudioWysoki").value = localStorage.getItem("HP-AudioWysoki")??'';
-        document.querySelector("#HP-AudioKrytyczny").value = localStorage.getItem("HP-AudioKrytyczny")??'';
-        document.querySelector("#HP-AudioBloker").value = localStorage.getItem("HP-AudioBloker")??'';
-        document.querySelector("#HP-AudioAwaria").value = localStorage.getItem("HP-AudioAwaria")??'';
-        document.querySelector("#HP-OpenLayout").checked = localStorage.getItem("HP-OpenLayout")??'';
+        //document.querySelector("#HP-AudioNiski").value = localStorage.getItem("HP-AudioNiski")??'';
+        //document.querySelector("#HP-AudioWysoki").value = localStorage.getItem("HP-AudioWysoki")??'';
+        //document.querySelector("#HP-AudioKrytyczny").value = localStorage.getItem("HP-AudioKrytyczny")??'';
+        //document.querySelector("#HP-AudioBloker").value = localStorage.getItem("HP-AudioBloker")??'';
+        //document.querySelector("#HP-AudioAwaria").value = localStorage.getItem("HP-AudioAwaria")??'';
+        //document.querySelector("#HP-OpenLayout").checked = localStorage.getItem("HP-OpenLayout")??'';
+        //document.querySelector("#HP-PrintLayout").checked = localStorage.getItem("HP-PrintLayout")??'';
+
 
     }
 
